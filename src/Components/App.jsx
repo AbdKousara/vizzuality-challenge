fe8bfc2e7f7bf36301b8690d5e643f6c50bb4071
AbdoKousara <abdelkader.kawsara@gmail.com>
2021-12-09 13:25:51 +0100

    minor fix

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,33 +1,58 @@
 import React from "react";
-import { useState } from "react";
-import {Basic} from './Basic.jsx';
+import { useState , useEffect} from "react";
+import { Basic } from "./Basic.jsx";
 import { Choropleth } from "./Choropleth.jsx";
-import {Gradient} from './Gradient.jsx'
-const fetchData = fetch("https://raw.githubusercontent.com/Vizzuality/front-end-code-challenge/master/data.json").then((response) => response.json())
-
-export default function App(){
-       const [data, setData] = useState([]);
-       fetchData.then((data) => setData(data));
-
-       return(
-              <div>
-                    {     
-                       data.filter((item)=>{
-                          return item.type !== 'timeline'
-                       }).reverse().map((item)=>{ 
-                          return(    
-                            item.type === 'basic' ? <Basic key={item.id} name={item.name} items={item.items} description={item.description}/> :
-                            item.type === 'choropleth' ? <Choropleth key={item.id} name={item.name} items={item.items} description={item.description}/> :
-                            <Gradient key={item.id} name={item.name} items={item.items} description={item.description}/> )
-                              
-                       })
-                              
-                        
-                     }
+import { Gradient } from "./Gradient.jsx";
 
 
-              </div>
-       );
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+       async function fetchMyAPI() {
+         let response = await fetch("https://raw.githubusercontent.com/Vizzuality/front-end-code-challenge/master/data.json")
+         let josnResponse = await response.json();
+         setData(josnResponse)
+         console.log(josnResponse);
+       }
+   
+       fetchMyAPI()
+     }, [])
+
+  return (
+    <div>
+      {
+      
+
+      data.reverse().map((item, index) => {
+       return item.type !== "timeline" ?
+         (item.type === "basic" ? 
+        <Basic
+          key={index}
+          name={item.name}
+          items={item.items}
+          description={item.description}
+        />
+       : item.type === "choropleth" ? 
+        <Choropleth
+          key={index}
+          name={item.name}
+          items={item.items}
+          description={item.description}
+        />
+       : 
+        <Gradient
+          key={index}
+          name={item.name}
+          items={item.items}
+          description={item.description}
+        />) : null
        
-       
+      })
+
+
+      
+      }
+    </div>
+  );
 }
